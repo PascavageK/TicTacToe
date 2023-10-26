bf74248b3b2d528b19a6239090c8d2d0a806d900
Kevin <148909004+PascavageK@users.noreply.github.com>
2023-10-26 15:28:26 -0400

    Initial commit
    Extremely basic terminal game

---
+++ Tic-Tac-Toe/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+//Draws the tic tac toe board to the consol
+void drawBoard(char boardState[]) {
+    printf("\n\n%c | %c | %c\n---------", boardState[0], boardState[1], boardState[2]);
+    printf("\n%c | %c | %c\n---------", boardState[3], boardState[4], boardState[5]);
+    printf("\n%c | %c | %c", boardState[6], boardState[7], boardState[8]);
+}
+
+//Checks for a winner; returns: 0 if no winner, 1 if O won, 2 if X won
+int checkWin(char boardState[]) {
+    int checkO = 0;
+    int checkX = 0;
+
+    //Loop for verticle and horizantle win check
+    for (int i = 0; i < 3; i++) {
+        checkO = 0, checkX = 0;
+
+        //Loop for verticle win
+        for (int x = i; x < 8; x += 3) {
+            if (boardState[x] == 79) checkO += 1;
+            if (boardState[x] == 88) checkX += 1;
+        }
+        if (checkO == 3) {printf("vert 1"); return 1;}
+        else if (checkX == 3) return 2;
+        
+        checkO = 0, checkX = 0;
+
+        //Loop for horizantle win
+        if (i == 0) for (int x = 0; x < 3; x++) {
+            if (boardState[x] == 79) checkO += 1;    
+            if (boardState[x] == 88) checkX += 1;
+        }
+        else if (i == 1) for (int x = 3; x < 6; x++) {
+            if (boardState[x] == 79) checkO += 1;
+            if (boardState[x] == 88) checkX += 1;
+        }
+        else for (int x = 6; x < 9; x++) {
+            if (boardState[x] == 79) checkO += 1;
+            if (boardState[x] == 88) checkX += 1;
+        }
+        if (checkO == 3) return 1;
+        else if (checkX == 3) return 2;
+    }
+    
+    checkO = 0, checkX = 0;
+
+    //Cross checks
+    if ((boardState[0]+boardState[4]+boardState[8])%79 == 0 ||
+        (boardState[2]+boardState[4]+boardState[6])%79 == 0) return 1;
+    else if ((boardState[0]+boardState[4]+boardState[8])%88 == 0 ||         
+             (boardState[2]+boardState[4]+boardState[6])%88 == 0) return 2;
+
+    //If no winner
+    return 0;
+}
+
+int playerMove(char player) {
+    int space;
+
+    printf("\n\nPlayer %c:", player);
+    scanf("%d", &space);
+
+    return space;
+}
+
+//main!
+int main() {
+    //Board
+    char boardState[] = {48, 49, 50, 51, 52, 53, 54, 55, 56};
+
+    //Game loop
+    for (int i = 0; i < 4; i++) {
+        drawBoard(boardState);
+        boardState[playerMove(88)] = 88;
+        drawBoard(boardState);
+        if (checkWin(boardState) == 1) {printf("\n\nPlayer O wins!"); break;}
+        boardState[playerMove(79)] = 79;
+        drawBoard(boardState);
+        if (checkWin(boardState) == 2) {printf("\n\nPlayer X wins!"); break;}
+    }
+    
+
+    return 0;
+}
